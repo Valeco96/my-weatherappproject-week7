59021cadb589de835751dd0d95507dc0fe3a6440
Valeco96 <correalev2@gmail.com>
2025-01-08 14:40:10 +0100

    Added JS submit value

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function enterCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-bar-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form-input");
+searchFormElement.addEventListener("submit", enterCity);
