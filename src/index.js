208938a907c59b21d5bccfa4458c7682ca94ee87
Valeco96 <correalev2@gmail.com>
2025-01-08 15:19:43 +0100

    Added API connection and call functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function insertMeteo(response) {
+  let temperatureElement = document.querySelector("#temperature-input");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "5f6791e5e0b8c3b470of06t31a2937df";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(insertMeteo);
+}
+
 function enterCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-bar-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form-input");
 searchFormElement.addEventListener("submit", enterCity);
+
+searchCity("Rome");
