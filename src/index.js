e179c0957a68185b5562b1c5de10f219048822e3
Valeco96 <correalev2@gmail.com>
2025-01-10 18:59:38 +0100

    Added the icon connected to the APIs

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ function displayForecast(response) {
       `
   <div class="weather-forecast-day">
     <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">⛅</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
         <strong>${Math.round(day.temperature.maximum)}°</strong>
